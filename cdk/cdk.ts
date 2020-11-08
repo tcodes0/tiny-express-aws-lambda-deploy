5af0bcf5706ec5b2d1e5594f62eeef086d912f23
Raphael Thomazella <thomazella9@gmail.com>
2020-11-08 19:42:41 -0300

    chore(misc): add app instance, format code

--- cdk/cdk.ts
+++ cdk/cdk.ts
@@ -1,46 +1,57 @@
-import * as cdk from '@aws-cdk/core';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as apigateway from '@aws-cdk/aws-apigateway';
-import * as cloudfront from '@aws-cdk/aws-cloudfront';
-import * as path from "path";
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigateway from '@aws-cdk/aws-apigateway'
+import * as cloudfront from '@aws-cdk/aws-cloudfront'
+import * as path from 'path'
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
     // lambda
-    const fn = new lambda.Function(this, "MyLambda", {
+    const fn = new lambda.Function(this, 'MyLambda', {
       runtime: lambda.Runtime.NODEJS_12_X,
-      handler: "build/lambda.handler",
-      code: lambda.Code.fromAsset(path.join(__dirname, "../../"), {
-        exclude: ["cdk"]
-      })
-    });
+      handler: 'build/lambda.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../'), {
+        exclude: ['cdk'],
+      }),
+    })
 
     // ApiGW
-    const apigw = new apigateway.LambdaRestApi(this, "MyApi", {
+    const apigw = new apigateway.LambdaRestApi(this, 'MyApi', {
       handler: fn,
-      proxy: true
-    });
+      proxy: true,
+    })
 
     // CF
-    const feCf = new cloudfront.CloudFrontWebDistribution(this, "MyCf", {
-      defaultRootObject: "/",
-      originConfigs: [{
-        customOriginSource: {
-          domainName: `${apigw.restApiId}.execute-api.${this.region}.${this.urlSuffix}`,
+    const feCf = new cloudfront.CloudFrontWebDistribution(this, 'MyCf', {
+      defaultRootObject: '/',
+      originConfigs: [
+        {
+          customOriginSource: {
+            domainName: `${apigw.restApiId}.execute-api.${this.region}.${this.urlSuffix}`,
+          },
+          originPath: '/' + apigw.deploymentStage.stageName,
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+            },
+          ],
         },
-        originPath: '/' + apigw.deploymentStage.stageName,
-        behaviors: [{
-          isDefaultBehavior: true,
-        }]
-      }],
+      ],
       enableIpV6: true,
-    });
+    })
 
     // Output
-    new cdk.CfnOutput(this, "myOut", {
-      value: feCf.domainName
-    });
+    new cdk.CfnOutput(this, 'myOut', {
+      value: feCf.domainName,
+    })
   }
 }
+
+const app = new cdk.App()
+new CdkStack(app, 'CdkStack', {
+  env: {
+    region: 'eu-central-1',
+  },
+})

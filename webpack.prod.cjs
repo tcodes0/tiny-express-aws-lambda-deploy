56b3715bf5dbcfc427d19da6fbb7a838460dd6ee
Raphael Thomazella <thomazella9@gmail.com>
2020-11-08 19:51:31 -0300

    chore(misc): add webpack add tscofngi

---
+++ webpack.prod.cjs
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.cjs')
+
+const prodConfig = {}
+
+prodConfig.mode = 'none'
+prodConfig.entry = {
+  app: './app/index.ts',
+}
+prodConfig.plugins = []
+const config = merge(common, prodConfig)
+
+module.exports = config
